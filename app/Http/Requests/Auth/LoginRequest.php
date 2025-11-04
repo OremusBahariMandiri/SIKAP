4d2c85b7bfb6acb5ef6a61dfc0747d5a035c0c8b
Jamas108 <jamasguitaris@gmail.com>
2025-11-04 10:31:19 +0700

    cptch

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -29,7 +29,7 @@ class LoginRequest extends FormRequest
         return [
             'nik' => ['required', 'string'],
             'password' => ['required', 'string'],
-            'g-recaptcha-response' => ['required', 'captcha'],
+            'g-recaptcha-response' => ['required', new RecaptchaRule],
         ];
     }
 
